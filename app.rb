ccd976c616de010f911270cc8ee2852d8667e6f9
Philip Lee-Chin <phil.leechin@outlook.com>
2020-05-06 22:46:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Lee."
+  end
+
+  get '/hometown' do
+    "My hometown is Jamaica W.I."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Usher - Rivals'"
+  end
 end
