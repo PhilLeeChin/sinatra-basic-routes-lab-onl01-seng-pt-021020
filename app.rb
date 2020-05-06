d3d4f54a26cd0f73834045900223a481560fab6b
Philip Lee-Chin <phil.leechin@outlook.com>
2020-05-06 22:45:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    'My name is Lee.'
+    "My name is Lee."
   end
 
   get '/hometown' do
-
+    "My hometown is Jamaica W.I."
   end
 end
